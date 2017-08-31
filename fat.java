e65af3341136a40454e345327bbb320f950c4e22
gowri06 <gowripadmanathan@gmail.com>
2017-08-31 11:14:23 +0530

    Update and rename fact.java to fat.java

---
+++ fat.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class fat
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int f=1,u;
+int a=s.nextInt();
+for(int i=1;i<a;i++)
+{
+f=f*i;
+System.out.println(f);
+}
+}
+}
